4cc4c7e699aea5b69e74599d1c2a1b39b282a651
David Vasilevski <david@Davids-MacBook-Pro.local>
2023-03-01 23:54:06 +0800

    Add form to addTask

--- src/Components/AddTask.tsx
+++ src/Components/AddTask.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Tile from "./Tile";
 import { AiOutlinePlus } from "react-icons/ai";
 
@@ -7,31 +7,35 @@ interface AddTaskProps {
 }
 
 export default function AddTask({ addTask }: AddTaskProps) {
-    const [ name, setName ] = useState<string>();
+    const [name, setName] = useState<string>();
+
+    const onSubmit = (event: React.FormEvent) => {
+        if (name) {
+            addTask(name);
+            setName('');
+        }
+        event.preventDefault();
+    };
 
     return (
         <Tile>
-            <div style={
-                {
-                    display: "grid", gridTemplateColumns: "1fr 40px", gridGap: "40px"
-                }
-            }>
-                
+            <form onSubmit={onSubmit}
+                style={
+                    {
+                        display: "grid", gridTemplateColumns: "1fr 40px", gridGap: "40px"
+                    }
+                }>
+
                 <input style={
-                    {verticalAlign: "middle", borderRadius: "8px", border: "1px solid #4e72cc", fontSize: "16px", width: "100%", padding: "8px"}
+                    { verticalAlign: "middle", borderRadius: "8px", border: "1px solid #4e72cc", fontSize: "16px", width: "100%", padding: "8px" }
                 } value={name} onChange={(event) => {
                     setName(event.target.value);
                 }}></input>
                 <AiOutlinePlus className='button' style={
-                    {borderRadius: "8px", border: "1px solid #4e72cc", width: "40px", height: "40px"}
-                } onClick={() => {
-                    if (name) {
-                        addTask(name);
-                        setName('');  
-                    }
-                }}>
+                    { borderRadius: "8px", border: "1px solid #4e72cc", width: "40px", height: "40px" }
+                } onClick={onSubmit}>
                 </AiOutlinePlus>
-            </div>
+            </form>
         </Tile>
     );
 }
